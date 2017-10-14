4df66090a7a15d7e22b8ff60d967c8aac4bafb8b
Annebelle Olminkhof <annebelleolminkhof@gmail.com>
2017-10-14 02:01:59 +0000

    simplified ruby, added nokogiri gem

--- models/model.rb
+++ models/model.rb
@@ -3,21 +3,21 @@ require 'open-uri'
 require 'nokogiri'
 require 'date'
 
-today = Date.today
+today = Date.today.strftime("%B %d")
 site = open("http://www.orangecounty.net/html/events.html")
 doc = Nokogiri::HTML(site)
 
-def get_events(doc)
-    i = 0
+def get_events(doc, date)
     events_array = Array.new
-    doc.css('font').each do |event|
+    doc.css('p').each do |event|
         new_event = event.text.gsub(/\s+/, " ")
-        events_array.push(new_event)
-        i += 1
-        puts new_event if new_event.include?("October ")
+        events_array.push(new_event) if new_event.include?(date)
     end
+    puts events_array
 end
 
-get_events(doc)
+get_events(doc, today)
+# puts today
 
 # puts today
+# also get links for events 'b > a'
