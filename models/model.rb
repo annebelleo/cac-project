08c3d1472579045f05d31e11db443dc08af7402c
Annebelle Olminkhof <annebelleolminkhof@gmail.com>
2017-10-17 05:10:41 +0000

    implementation of previous month

--- models/model.rb
+++ models/model.rb
@@ -7,21 +7,32 @@ require 'nokogiri'
 require 'date'
 
 def get_first_event
+    #date variables
     day = Date.today.strftime("%d")
-    # day = "15"
-    month = Date.today.strftime("%B")
+    month_name = Date.today.strftime("%B")
+    d = Date.today
+    last_month = d << 1
+    prev_month = last_month.strftime("%B")
+    
+    #parsing
     site = open("http://www.orangecounty.net/html/events.html")
     doc = Nokogiri::HTML(site)
     events_array = Array.new
     doc.css('p').each do |event|
         event = event.text.gsub(/\s+/, " ")
-        events_array.push(event) if event.include?(month) && event.include?(day)
+        events_array.push(event) if event.include?(month_name) && event.include?(day)
     end
+    
     if events_array.empty? == false
         events_array.each do |event|
-            return event.split(month)[0]
+            if event.include?(prev_month)
+                new_event = event.split(prev_month)[0]
+            else
+                new_event = event.split(month_name)[0]
+            end
+            return new_event
         end
-        events_array.push(event)
+        events_array.push(new_event)
         return events_array[0]
     else
         message = "There are no events today."
@@ -31,18 +42,30 @@ def get_first_event
 end
 
 def get_second_event
+    
+    #date variables
     day = Date.today.strftime("%d")
-    # day = "15"
-    month = Date.today.strftime("%B")
+    month_name = Date.today.strftime("%B")
+    d = Date.today
+    last_month = d << 1
+    prev_month = last_month.strftime("%B")
+    
+    #parsing
     site = open("http://www.orangecounty.net/html/events.html")
     doc = Nokogiri::HTML(site)
     events_array = Array.new
     doc.css('p').each do |event|
         event = event.text.gsub(/\s+/, " ")
-        events_array.push(event) if event.include?(month) && event.include?(day)
+        events_array.push(event) if event.include?(month_name) && event.include?(day)
     end
+    
     if events_array.empty? == false
-        new_event = events_array[1].split(month)[0]
+        event1 = events_array[1]
+        if event1.include?(prev_month)
+            new_event = event1.split(prev_month)[0]
+        else
+            new_event = event1.split(month_name)[0]
+        end
         events_array.clear
         events_array.push(new_event)
         return events_array[0]
--- models/test.rb
+++ models/test.rb
@@ -6,3 +6,8 @@ def get_links
     doc = Nokogiri::HTML(site)
     puts doc.css('b > a')
 end
+
+d = Date.today
+last_month = d << 1
+
+puts last_month.strftime("%B")
