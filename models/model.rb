262ff8ad09f5b1fe626286230b0ecba91c41841a
Annebelle Olminkhof <annebelleolminkhof@gmail.com>
2017-10-26 20:46:29 +0000

    the second event isn't working for today

--- models/model.rb
+++ models/model.rb
@@ -43,46 +43,46 @@ def get_first_event
     end
 end
 
-def get_second_event
+# def get_second_event
     
-    #date variables
-    d = Time.now.getlocal("-07:00") 
-    day = d.strftime("%d")
-    # day = "17"
-    month_name = d.strftime("%B")
-    today = Date.today
-    last_month = today << 1
-    prev_month = last_month.strftime("%B")
+#     #date variables
+#     d = Time.now.getlocal("-07:00") 
+#     day = d.strftime("%d")
+#     # day = "17"
+#     month_name = d.strftime("%B")
+#     today = Date.today
+#     last_month = today << 1
+#     prev_month = last_month.strftime("%B")
     
-    #parsing
-    site = open("http://www.orangecounty.net/html/events.html")
-    doc = Nokogiri::HTML(site)
-    events_array = Array.new
-    doc.css('p').each do |event|
-        event = event.text.gsub(/\s+/, " ")
-        events_array.push(event) if event.include?(month_name) && event.include?(day)
-    end
+#     #parsing
+#     site = open("http://www.orangecounty.net/html/events.html")
+#     doc = Nokogiri::HTML(site)
+#     events_array = Array.new
+#     doc.css('p').each do |event|
+#         event = event.text.gsub(/\s+/, " ")
+#         events_array.push(event) if event.include?(month_name) && event.include?(day)
+#     end
     
-    if events_array.empty? == false
-        event1 = events_array[1]
-        if event1.include?(prev_month)
-            new_event = event1.split(prev_month)[0]
-        else
-            new_event = event1.split(month_name)[0]
-        end
-        events_array.clear
-        events_array.push(new_event)
-        return events_array[0]
-    else
-        message = " "
-        events_array.push(message)
-        return events_array[0]
-    end
-end
+#     if events_array.empty? == false
+#         event1 = events_array[1]
+#         if event1.include?(prev_month)
+#             new_event = event1.split(prev_month)[0]
+#         else
+#             new_event = event1.split(month_name)[0]
+#         end
+#         events_array.clear
+#         events_array.push(new_event)
+#         return events_array[0]
+#     else
+#         message = " "
+#         events_array.push(message)
+#         return events_array[0]
+#     end
+# end
 
 def get_date
     d = Time.now.getlocal("-07:00")
     return d.strftime("%B %d, %Y")
 end
 
-#CURRENT GOAL: generate random events occurring in a day
+
