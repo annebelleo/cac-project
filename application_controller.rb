cb07199bd54038f85c00a959c36c4674241d3db1
Taylor Want <taylor.want@gmail.com>
2017-10-30 20:00:31 +0000

    add iteration

--- application_controller.rb
+++ application_controller.rb
@@ -4,8 +4,12 @@ Bundler.require
 require_relative 'models/model.rb'
 
 class MyApp < Sinatra::Base
-
   get '/' do
+    @cities=["http://www.orangecounty.net/html/events.html", "http://www.cityofalisoviejo.com/city-hall/departments/parks-recreation/special-events/", "http://www.anaheim.net/calendar.aspx?CID=14&showPastEvents=false", "http://www.ci.brea.ca.us/index.aspx?NID=448", "http://www.cypressca.org/play/event-calendar", "http://www.danapoint.org/residents/events-activities", "http://cityofmissionviejo.org/events?combine=&field_date_value_1%5Bvalue%5D%5Bdate%5D=&field_date_value2%5Bvalue%5D%5Bdate%5D=&field_event_category_tid=78"]
+    @data=[]
+    @cities.each do |city_site|
+      @data.push(get_event("http://www.cityofalisoviejo.com/city-hall/departments/parks-recreation/special-events/"))
+    end
     erb :index
   end
   
--- models/model.rb
+++ models/model.rb
@@ -5,8 +5,7 @@ require 'open-uri'
 require 'nokogiri'
 require 'date'
 
-def get_first_event
-    
+def get_event(website)
     #date variables
     d = Time.now.getlocal("-07:00") 
     day = d.strftime("%d")
@@ -17,7 +16,7 @@ def get_first_event
     prev_month = last_month.strftime("%B")
     
     #parsing
-    site = open("http://www.orangecounty.net/html/events.html")
+    site = open(website)
     doc = Nokogiri::HTML(site)
     events_array = Array.new
     doc.css('p').each do |event|
@@ -43,46 +42,46 @@ def get_first_event
     end
 end
 
-def get_second_event
+# def get_second_event
     
-    #date variables
-    d = Time.now.getlocal("-07:00") 
-    day = d.strftime("%d")
-    # day = "17"
-    month_name = d.strftime("%B")
-    today = Date.today
-    last_month = today << 1
-    prev_month = last_month.strftime("%B")
+#     #date variables
+#     d = Time.now.getlocal("-07:00") 
+#     day = d.strftime("%d")
+#     # day = "17"
+#     month_name = d.strftime("%B")
+#     today = Date.today
+#     last_month = today << 1
+#     prev_month = last_month.strftime("%B")
     
-    #parsing
-    site = open("http://www.orangecounty.net/html/events.html")
-    doc = Nokogiri::HTML(site)
-    events_array = Array.new
-    doc.css('p').each do |event|
-        event = event.text.gsub(/\s+/, " ")
-        events_array.push(event) if event.include?(month_name) && event.include?(day)
-    end
+#     #parsing
+#     site = open("http://www.orangecounty.net/html/events.html")
+#     doc = Nokogiri::HTML(site)
+#     events_array = Array.new
+#     doc.css('p').each do |event|
+#         event = event.text.gsub(/\s+/, " ")
+#         events_array.push(event) if event.include?(month_name) && event.include?(day)
+#     end
     
-    if events_array.length > 1
-        event1 = events_array[1]
-        if event1.exclude?(prev_month)
-            message = " "
-            events_array.push(message)
-        elsif event1.include?(prev_month)
-            new_event = event1.split(prev_month)[0]
-        else
-            new_event = event1.split(month_name)[0]
-        end
-        events_array.clear
-        events_array.push(new_event)
-        return events_array[0]
-    else
-        events_array.clear
-        message = " "
-        events_array.push(message)
-        return events_array[0]
-    end
-end
+#     if events_array.length > 1
+#         event1 = events_array[1]
+#         if event1.exclude?(prev_month)
+#             message = " "
+#             events_array.push(message)
+#         elsif event1.include?(prev_month)
+#             new_event = event1.split(prev_month)[0]
+#         else
+#             new_event = event1.split(month_name)[0]
+#         end
+#         events_array.clear
+#         events_array.push(new_event)
+#         return events_array[0]
+#     else
+#         events_array.clear
+#         message = " "
+#         events_array.push(message)
+#         return events_array[0]
+#     end
+# end
 
 def get_date
     d = Time.now.getlocal("-07:00")
